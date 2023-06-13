b3ea6c6d4fd4a9f16413d462ba30f7cff1b58ccc
Albert <albertvitt1997@gmail.com>
2023-06-14 02:12:43 +0700

    serializer_title_genre_category

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+from reviews.models import (Titles, Genres, Categories)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    """Сериализатор категорий"""
+    class Meta:
+        fields = ['name',
+                  'slug'
+                  ]
+        look_up_field = 'slug'
+        model = Categories
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    """Сериализатор жанров"""
+    class Meta:
+        fields = ['name',
+                  'slug'
+                  ]
+        look_up_field = 'slug'
+        model = Genres
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    """Тайтл сериализатор"""
+    category = SlugRelatedField(slug_field='slug',
+                                read_only=True
+                                )
+    genre = SlugRelatedField(slug_field='slug',
+                             read_only=True
+                             )
+
+    class Meta:
+        fields = '__all__'
+        model = Titles
