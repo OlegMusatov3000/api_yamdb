07b157b0ae22bbd435f88f8718d157017fb997fd
ALbert <albertvitt1997@gmail.com>
2023-06-18 19:54:45 +0300

    add_perm_adminOr_Read

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -22,8 +22,32 @@ class IsSuperUserOrAdminOrModeratorOrAuthorOrReadOnly(BasePermission):
         return (
             request.method in SAFE_METHODS
             or request.user.is_authenticated and (
-                request.user.is_stuff
+                request.user.is_staff
                 or request.user.role == 'admin'
                 or request.user.role == 'moderator'
                 or request.user == obj.author
             ))
+
+
+class IsAdminModeratorOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (request.method in SAFE_METHODS
+                or request.user.role == 'admin'
+                or request.user.role == 'moderator'
+                or obj.author == request.user)
+
+    def has_permission(self, request, view):
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated)
+
+
+class IsAdminOrReadOnly(BasePermission):
+    """Права для Администратора и Суперпользователя Django"""
+    message = MESSAGE
+
+    def has_permission(self, request, view):
+        return (request.method in SAFE_METHODS or
+                request.user.is_authenticated and (
+                    request.user.role == 'admin' or
+                    request.user.is_staff
+                ))
